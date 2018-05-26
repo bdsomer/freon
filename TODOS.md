a2d68d09614bc2a19641832ebc1e3e75ebc2ee51
Bennett Somerville <bdsomer@gmail.com>
2018-05-25 21:00:49 -0700

    Initial Docs Commit

--- TODOS.md
+++
@@ -1,3 +0,0 @@
-# TODOs
-
-- Create something along the lines of `res.upload`
--- tests/test.js
+++
@@ -1,9 +0,0 @@
-const tl = require('testlite');
-
-tl('Application', require('./applicationTest.js'));
-tl('Should Compress Module', require('./shouldCompressTest.js'));
-tl('Request Object', require('./requestObjectTest.js'));
-tl('Request Object - Accept', require('./requestObjectAcceptTest.js'));
-tl('Response Object', require('./responseObjectTest.js'));
-tl('Static', require('./staticTest.js'));
-tl.test();
